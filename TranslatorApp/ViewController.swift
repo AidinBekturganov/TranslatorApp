0cb59830a1f75a88a5ecf686229efc917cf15a44
Aidin <bekturganovaidin@gmail.com>
2020-08-09 21:19:16 +0600

    Initial Commit

---
+++ TranslatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TranslatorApp
+//
+//  Created by User on 8/9/20.
+//  Copyright © 2020 Aidin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
